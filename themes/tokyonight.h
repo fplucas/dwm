3291a74fd79d27d6753e82f75661945328b1d42b
Lucas Pugliesi <ferreiraplucas@gmail.com>
2024-04-21 12:47:12 +0100

    First commit

---
+++ themes/tokyonight.h
@@ -0,0 +1,13 @@
+#ifndef TOKYONIGHT_H
+#define TOKYONIGHT_H
+
+static const char black[]   = "#24283b";
+static const char red1[]    = "#ff7a93";
+static const char green[]   = "#b9f27c";
+static const char yellow[]  = "#e0af68";
+static const char blue[]    = "#7aa2f7";
+static const char magenta[] = "#bb9af7";
+static const char cyan[]    = "#6cd8ff";
+static const char white[]   = "#f6f0e5";
+
+#endif
